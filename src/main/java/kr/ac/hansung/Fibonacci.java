c340ebbbc28adae85bee00464bdfc95c318bb68a
songhm7 <songhm7@naver.com>
2024-11-18 03:14:50 +0900

    오타수정

--- src/main/java/kr/ac/hansung/Fibonacci.java
+++ src/main/java/kr/ac/hansung/Fibonacci.java
@@ -15,7 +15,7 @@ public class Fibonacci {
 
         for (int i = 0; i < n; i++) {
             //System.out.print(fibonacci(i) + " ");
-            int fib = fibonacchi(i);
+            int fib = fibonacci(i);
             System.out.print("fibonacci(" + i + ")=" + fib);
             System.out.println();
         }
